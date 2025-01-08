786f70bcab39bfc208f1b52fb91438265ca0a73b
Eddy <eddienyagano@gmail.com>
2025-01-08 16:01:25 +0200

    Added material langauge selector

--- apps/collections/src/app/components/navbar/navbar.component.ts
+++ apps/collections/src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectModule, MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-navbar',
@@ -12,11 +12,14 @@ import { MatSelectModule } from '@angular/material/select';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  constructor(private translate: TranslateService) {}
+  selectedLanguage = 'en';
 
-  onLanguageChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    const language = selectElement.value;
-    this.translate.use(language);
+  constructor(private translate: TranslateService) {
+    this.translate.use(this.selectedLanguage);
+  }
+
+  onLanguageChange(event: MatSelectChange) {
+    const selectedLanguage = event.value;
+    this.translate.use(selectedLanguage);
   }
 }
