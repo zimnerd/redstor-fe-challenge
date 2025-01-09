786bcd89ce1a35ccafd52940d6cfbfac2c67111f
Eddy Ganz <eddienyagano@gmail.com>
2025-01-09 15:09:45 +0200

    Added breadcrumbs to navbar

--- apps/collections/src/app/components/navbar/navbar.component.ts
+++ apps/collections/src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal, signal } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule, MatSelectChange } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+
+interface Breadcrumb {
+  label: string;
+  url?: string;
+}
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatSelectModule, TranslateModule],
+  imports: [CommonModule, MatToolbarModule, MatSelectModule, TranslateModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  selectedLanguage = 'en';
+  @Input() breadcrumbs: Breadcrumb[] = [];
+  selectedLanguage = signal(localStorage.getItem('selectedLanguage') || 'en');
 
   constructor(private translate: TranslateService) {
-    this.translate.use(this.selectedLanguage);
+    this.translate.use(this.selectedLanguage());
   }
 
   onLanguageChange(event: MatSelectChange) {
-    const selectedLanguage = event.value;
-    this.translate.use(selectedLanguage);
+    this.selectedLanguage.set(event.value);
+    this.translate.use(this.selectedLanguage());
+    localStorage.setItem('selectedLanguage', this.selectedLanguage());
   }
 }
